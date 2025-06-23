3b532fa9c8e6cde71b81da5369af8d9752340f1b
chonkachu <chrissimascgr@gmail.com>
2025-06-22 21:49:56 -0300

    adicionando COMO_RODAR e modificando clock

--- demo/FreeRTOSConfig.h
+++ demo/FreeRTOSConfig.h
@@ -46,7 +46,7 @@
 #define configUSE_PREEMPTION                    ( 1 )
 #define configUSE_IDLE_HOOK                     ( 1 )
 #define configUSE_TICK_HOOK                     ( 1 )
-#define configCPU_CLOCK_HZ                      ( 100000000 )
+#define configCPU_CLOCK_HZ                      ( 50000000 )
 #define configTICK_RATE_HZ                      ( (TickType_t)(100) )
 #define configMAX_PRIORITIES                    ( 5 )
 #define configMINIMAL_STACK_SIZE                ( (unsigned short)(128) )
